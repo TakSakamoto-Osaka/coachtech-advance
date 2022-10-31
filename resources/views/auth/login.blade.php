20d98e8c51534ccb5fb39d7ce8ad8a08f4997691
Takayuki Sakamoto <k.sammohung@gmail.com>
2022-10-31 13:42:34 +0900

    urlパス設定にasset関数を用いる変更適用

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 <!-- ページ固有CSS -->
 @section('pageCSS')
-<link href="/css/login.css" rel="stylesheet">
+<link href="{{ asset('/css/login.css') }}" rel="stylesheet">
 @endsection
 
 <!-- ページコンテンツ部 -->
@@ -18,14 +18,14 @@
         
         <!-- Email Address -->
         <div class="email-block">
-            <img class="icon" src="/img/email.png" alt="">
+            <img class="icon" src="{{ asset('/img/email.png') }}" alt="">
             <input id="email" class="email-input" type="email" name="email" :value="old('email')" placeholder="Email" required autofocus />
             <p class="error"> @if($errors->has('email')) {{ $errors->first('email') }} @else &nbsp;  @endif </p>
         </div>
 
         <!-- Password -->
         <div class="password-block">
-            <img class="icon" src="/img/lock.png" alt="">
+            <img class="icon" src="{{ asset('/img/lock.png') }}" alt="">
             <input id="password" class="password-input"
                                 type="password"
                                 name="password"
@@ -38,8 +38,8 @@
             <button class="login-btn">ログイン</button>
         </div>
         <div class="forgot-password">
-            <a href="/register">新規登録</a>
-            <a href="/forgot-password">パスワードを忘れた方</a>
+            <a href="{{ asset('/register') }}">新規登録</a>
+            <a href="{{ asset('/forgot-password') }}">パスワードを忘れた方</a>
         </div>
 
     </form>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/css/reset.css" rel="stylesheet">
-  <link href="/css/common.css" rel="stylesheet">
+  <link href="{{ asset('/css/reset.css') }}" rel="stylesheet">
+  <link href="{{ asset('/css/common.css') }}" rel="stylesheet">
   @yield('pageCSS')
   <title>Rese 飲食店予約サイト</title>
 </head>
