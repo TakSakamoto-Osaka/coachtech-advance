96f5ad409316edf0598b0f97c295de1349236a12
Takayuki Sakamoto <k.sammohung@gmail.com>
2022-11-18 05:05:07 +0000

    CircleCI設定ホームディレクトリ修正 不要ファイル削除

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+git pull origin
+sudo rm -rf /var/www/coachtech-advance
+sudo cp -r ~/prog/coachtech-advance /var/www/
+sudo chown -R apache:apache /var/www/coachtech-advance
+sudo chmod -R 755 /var/www/coachtech-advance
+sudo systemctl restart httpd
+
+
+
+
+
+
