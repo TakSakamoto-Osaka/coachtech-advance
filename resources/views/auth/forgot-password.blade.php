424591c51ed62e44770665321e2631f16ea25405
Takayuki Sakamoto <k.sammohung@gmail.com>
2022-11-18 13:27:08 +0900

    パスワードリセット用メール送信画面カスタマイズ

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,35 +1,42 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends("layouts.default")
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!-- ページ固有CSS -->
+@section('pageCSS')
+<link href="{{ asset('/css/forgot-password.css') }}" rel="stylesheet">
+@endsection
+
+<!-- ページコンテンツ部 -->
+@section('content')
+
+<!-- Session Status -->
+<div class="forgot-password-outer" />
+    <div class="forgot-password-inner">
+        <div class="forgot-password-message">
+            パスワードを再設定する場合、下記メールアドレス欄にメールアドレスを入力し、『 パスワードリセットボタン』を押下してください 
         </div>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <div :status="session('status')" />
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
             <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <div class="email-block">
+                <div class="input-title">メールアドレス</div>
+                <input id="email" class="email-input" type="email" name="email" :value="old('email')" required autofocus/>
             </div>
+            <p class="error"> @if($errors->has('email')) {{ $errors->first('email') }} @else &nbsp;  @endif </p>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-primary-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-primary-button>
-            </div>
+            <button class="forgot-password-btn">
+                パスワードリセット
+            </button>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+
+</div>
+
+@endsection
+
+
+
